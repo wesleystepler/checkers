868ae946937c37f34d3bfdc4140d6707491d00a1
Wesley Stepler <pwesleystep@outlook.com>
2023-05-31 13:04:37 -0400

    Pieces are now displayed properly on the board

--- checkers.py
+++ checkers.py
@@ -1,19 +1,52 @@
 import pygame
+import os
+from pieces import BlackKing, BlackPawn, RedKing, RedPawn
+
+
+# Set up the Display
+pygame.init()
+pygame.display.set_caption("Checkers")
+
+WIDTH = 700
+HEIGHT = 700
+FPS = 60
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+red_pawn = RedPawn(50, 50)
+red_king = RedKing(300, 50)
+black_pawn = BlackPawn(50, 650)
+black_king = BlackKing(100, 650)
+
+red_pieces = pygame.sprite.Group()
+red_pieces.add(red_pawn)
+red_pieces.add(red_king)
+
+black_pieces = pygame.sprite.Group()
+black_pieces.add(black_pawn)
+black_pieces.add(black_king)
+
+# Method for drawing the screen
+def draw_window():
+  board = pygame.image.load("images/board.jpg").convert()
+  WIN.blit(board, (0, 0))
+  red_pieces.draw(WIN)
+  black_pieces.draw(WIN)
+  pygame.display.flip()
+  
 
 def main():
-  pygame.init()
-  pygame.display.set_caption("Checkers")
-
-  screen = pygame.display.set_mode((700, 700))
-  board = pygame.image.load("images/board.jpg").convert()
-  screen.blit(board, (0, 0))
-  pygame.display.flip()
+  clock = pygame.time.Clock()
 
   running = True
   while running:
+    clock.tick(FPS)
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
+
+    # We will be using this later when we implement moving pieces   
+    keys_pressed = pygame.key.get_pressed()
+    draw_window()
 
 if __name__ == "__main__":
   main()
--- pieces.py
+++ pieces.py
@@ -1,24 +1,41 @@
 import pygame
-class black_pawn(pygame.sprite.Sprite):
-    def __init__(self) -> None:
-        self.image, self.rect = pygame.load_image("images/black-pawn.png")    
-    def move():
-        pass
+import os
 
-class red_pawn(pygame.sprite.Sprite):
-    def __init__(self) -> None:
-        self.image, self.rect = pygame.load_image("images/red-pawn.png")    
-    def move():
-        pass
+# Load and scale images
+RED_PAWN_IMAGE = pygame.image.load(os.path.join('images', 'red-pawn.png'))
+RED_KING_IMAGE = pygame.image.load(os.path.join('images', 'red-king.png'))
+BLACK_PAWN_IMAGE = pygame.image.load(os.path.join('images', 'black-pawn.png'))
+BLACK_KING_IMAGE = pygame.image.load(os.path.join('images', 'black-king.png'))
 
-class black_king(pygame.sprite.Sprite):
-    def __init__(self):
-        self.image, self.rect = pygame.load_image("images/black-king.png")
-    def move():
-        pass
+RED_PAWN = pygame.transform.scale(RED_PAWN_IMAGE, (60, 60))
+RED_KING = pygame.transform.scale(RED_KING_IMAGE, (60, 60))
+BLACK_PAWN = pygame.transform.scale(BLACK_PAWN_IMAGE, (60, 60))
+BLACK_KING = pygame.transform.scale(BLACK_KING_IMAGE, (60, 60))
 
-class black_king(pygame.sprite.Sprite):
-    def __init__(self):
-        self.image, self.rect = pygame.load_image("images/red-king.png")
-    def move():
-        pass
+class RedPawn(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos) -> None:
+        super().__init__()
+        self.image = RED_PAWN
+        self.rect = self.image.get_rect()
+        self.rect.center = [x_pos, y_pos]
+
+class BlackPawn(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos) -> None:
+        super().__init__()
+        self.image = BLACK_PAWN
+        self.rect = self.image.get_rect()
+        self.rect.center = [x_pos, y_pos]
+
+class RedKing(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos) -> None:
+        super().__init__()
+        self.image = RED_KING
+        self.rect = self.image.get_rect()
+        self.rect.center = [x_pos, y_pos]
+
+class BlackKing(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos) -> None:
+        super().__init__()
+        self.image = BLACK_KING
+        self.rect = self.image.get_rect()
+        self.rect.center = [x_pos, y_pos]

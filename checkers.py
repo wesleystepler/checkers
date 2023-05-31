bf3ead1227d6054a7b7a195d42ffdd42015e076d
Wesley Stepler <pwesleystep@outlook.com>
2023-05-31 15:49:47 -0400

    All Pieces are now on the board

--- checkers.py
+++ checkers.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-from pieces import BlackKing, BlackPawn, CheckerSquare, RedKing, RedPawn, WOOD_TILE_1, WOOD_TILE_2
+from pieces import King, Pawn, CheckerSquare
 
 
 # Set up the Display
@@ -12,16 +12,46 @@ HEIGHT = 700
 FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# Initialize Board
-board = pygame.sprite.Group()
+# Load and scale images for the Board
+WOOD_TILE_IMAGE_1 = pygame.image.load(os.path.join('images', 'wood-tile-1.jpg'))
+WOOD_TILE_IMAGE_2 = pygame.image.load(os.path.join('images', 'wood-tile-2.jpg'))
+
+WOOD_TILE_1 = pygame.transform.scale(WOOD_TILE_IMAGE_1, (75, 75))
+WOOD_TILE_2 = pygame.transform.scale(WOOD_TILE_IMAGE_2, (75, 75))
+
+# Load and scale images for the pieces
+RED_PAWN_IMAGE = pygame.image.load(os.path.join('images', 'red-pawn.png'))
+RED_KING_IMAGE = pygame.image.load(os.path.join('images', 'red-king.png'))
+BLACK_PAWN_IMAGE = pygame.image.load(os.path.join('images', 'black-pawn.png'))
+BLACK_KING_IMAGE = pygame.image.load(os.path.join('images', 'black-king.png'))
+
+RED_PAWN = pygame.transform.scale(RED_PAWN_IMAGE, (60, 60))
+RED_KING = pygame.transform.scale(RED_KING_IMAGE, (60, 60))
+BLACK_PAWN = pygame.transform.scale(BLACK_PAWN_IMAGE, (60, 60))
+BLACK_KING = pygame.transform.scale(BLACK_KING_IMAGE, (60, 60))
+
 
 # Some variables we will use to loop through the initialization of the board
 cur_tile = WOOD_TILE_1
 x, y, = 150, 50
 step = 75
 
-# A two-dimensional list to use as a convenient reference to all the board sprites later
+""" A Two-Dimensional list which enables convenient referencing of each tile sprite. The board
+    is laid out in the list as follows:
+       0  1  2  3  4  5  6  7
+    0  X  X  X  X  X  X  X  X
+    1  X  X  X  X  X  X  X  X
+    2  X  X  X  X  X  X  X  X
+    3  X  X  X  X  X  X  X  X
+    4  X  X  X  X  X  X  X  X
+    5  X  X  X  X  X  X  X  X
+    6  X  X  X  X  X  X  X  X
+    7  X  X  X  X  X  X  X  X
+"""
 board_reference = []
+
+# Initialize Board and Pieces
+board = pygame.sprite.Group()
 for i in range(0, 8):
     row = []
     for j in range (0, 8):
@@ -39,25 +69,46 @@ for i in range(0, 8):
     board_reference.append(row)
 
 # Initialize Pieces
-red_pawn = RedPawn(50, 50)
-red_king = RedKing(300, 50)
-black_pawn = BlackPawn(50, 650)
-black_king = BlackKing(100, 650)
-
 red_pieces = pygame.sprite.Group()
-red_pieces.add(red_pawn)
-red_pieces.add(red_king)
-
 black_pieces = pygame.sprite.Group()
-black_pieces.add(black_pawn)
-black_pieces.add(black_king)
+
+for a in range(0, 2):
+    if a == 0:
+        i = 0
+        j = 1
+    else:
+        i = 1
+        j = 0
+
+    for b in range(0, 4):
+        piece = Pawn(0, 0, RED_PAWN)
+        piece.rect.center = board_reference[i][j].rect.center
+        red_pieces.add(piece)
+        j += 2
+
+for a in range(0, 2):
+    if a == 0:
+        i = 6
+        j = 1
+    else:
+        i = 7
+        j = 0
+
+    for b in range(0, 4):
+        piece = Pawn(0, 0, BLACK_PAWN)
+        piece.rect.center = board_reference[i][j].rect.center
+        black_pieces.add(piece)
+        j += 2
+
+
+        
 
 # Method for drawing the screen
 def draw_window():
   WIN.fill((1,50,32))
   board.draw(WIN)
-  #red_pieces.draw(WIN)
-  #black_pieces.draw(WIN)
+  red_pieces.draw(WIN)
+  black_pieces.draw(WIN)
   pygame.display.flip()
   
 
--- pieces.py
+++ pieces.py
@@ -1,24 +1,6 @@
 import pygame
 import os
 
-# Load and scale images for the Board
-WOOD_TILE_IMAGE_1 = pygame.image.load(os.path.join('images', 'wood-tile-1.jpg'))
-WOOD_TILE_IMAGE_2 = pygame.image.load(os.path.join('images', 'wood-tile-2.jpg'))
-
-WOOD_TILE_1 = pygame.transform.scale(WOOD_TILE_IMAGE_1, (75, 75))
-WOOD_TILE_2 = pygame.transform.scale(WOOD_TILE_IMAGE_2, (75, 75))
-
-# Load and scale images for the pieces
-RED_PAWN_IMAGE = pygame.image.load(os.path.join('images', 'red-pawn.png'))
-RED_KING_IMAGE = pygame.image.load(os.path.join('images', 'red-king.png'))
-BLACK_PAWN_IMAGE = pygame.image.load(os.path.join('images', 'black-pawn.png'))
-BLACK_KING_IMAGE = pygame.image.load(os.path.join('images', 'black-king.png'))
-
-RED_PAWN = pygame.transform.scale(RED_PAWN_IMAGE, (60, 60))
-RED_KING = pygame.transform.scale(RED_KING_IMAGE, (60, 60))
-BLACK_PAWN = pygame.transform.scale(BLACK_PAWN_IMAGE, (60, 60))
-BLACK_KING = pygame.transform.scale(BLACK_KING_IMAGE, (60, 60))
-
 class CheckerSquare(pygame.sprite.Sprite):
     def __init__(self, x_pos, y_pos, tile):
         super().__init__()
@@ -30,30 +12,16 @@ class CheckerSquare(pygame.sprite.Sprite):
         return f"Checker Square a position ({self.x_pos}, {self.y_pos})"
 
 
-class RedPawn(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos) -> None:
+class Pawn(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos, piece) -> None:
         super().__init__()
-        self.image = RED_PAWN
+        self.image = piece
         self.rect = self.image.get_rect()
         self.rect.center = [x_pos, y_pos]
 
-class BlackPawn(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos) -> None:
+class King(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos, piece) -> None:
         super().__init__()
-        self.image = BLACK_PAWN
-        self.rect = self.image.get_rect()
-        self.rect.center = [x_pos, y_pos]
-
-class RedKing(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos) -> None:
-        super().__init__()
-        self.image = RED_KING
-        self.rect = self.image.get_rect()
-        self.rect.center = [x_pos, y_pos]
-
-class BlackKing(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos) -> None:
-        super().__init__()
-        self.image = BLACK_KING
+        self.image = piece
         self.rect = self.image.get_rect()
         self.rect.center = [x_pos, y_pos]

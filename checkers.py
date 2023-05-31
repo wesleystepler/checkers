a1eaf049c36f337a7f6dbb9757698cd4b6cec950
Wesley Stepler <pwesleystep@outlook.com>
2023-05-31 18:49:20 -0400

    Add rect to cursor and work on move method

--- checkers.py
+++ checkers.py
@@ -2,7 +2,6 @@ import pygame
 import os
 from pieces import King, Pawn, CheckerSquare
 
-
 # Set up the Display
 pygame.init()
 pygame.display.set_caption("Checkers")
@@ -12,6 +11,13 @@ HEIGHT = 700
 FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
+#Create Rect object in place of normal mouse cursor
+pygame.mouse.set_visible(False)
+CURSOR_IMAGE = pygame.image.load(os.path.join('images', 'cursor.png'))
+CURSOR = pygame.transform.scale(CURSOR_IMAGE, (35, 35))
+
+cursor = CURSOR.get_rect()
+
 # Load and scale images for the Board
 WOOD_TILE_IMAGE_1 = pygame.image.load(os.path.join('images', 'wood-tile-1.jpg'))
 WOOD_TILE_IMAGE_2 = pygame.image.load(os.path.join('images', 'wood-tile-2.jpg'))
@@ -19,20 +25,9 @@ WOOD_TILE_IMAGE_2 = pygame.image.load(os.path.join('images', 'wood-tile-2.jpg'))
 WOOD_TILE_1 = pygame.transform.scale(WOOD_TILE_IMAGE_1, (75, 75))
 WOOD_TILE_2 = pygame.transform.scale(WOOD_TILE_IMAGE_2, (75, 75))
 
-# Load and scale images for the pieces
-RED_PAWN_IMAGE = pygame.image.load(os.path.join('images', 'red-pawn.png'))
-RED_KING_IMAGE = pygame.image.load(os.path.join('images', 'red-king.png'))
-BLACK_PAWN_IMAGE = pygame.image.load(os.path.join('images', 'black-pawn.png'))
-BLACK_KING_IMAGE = pygame.image.load(os.path.join('images', 'black-king.png'))
-
-RED_PAWN = pygame.transform.scale(RED_PAWN_IMAGE, (60, 60))
-RED_KING = pygame.transform.scale(RED_KING_IMAGE, (60, 60))
-BLACK_PAWN = pygame.transform.scale(BLACK_PAWN_IMAGE, (60, 60))
-BLACK_KING = pygame.transform.scale(BLACK_KING_IMAGE, (60, 60))
-
 
 # Some variables we will use to loop through the initialization of the board
-cur_tile = WOOD_TILE_1
+cur_tile = 'tile1'
 x, y, = 150, 50
 step = 75
 
@@ -69,6 +64,8 @@ for i in range(0, 8):
     board_reference.append(row)
 
 # Initialize Pieces
+# A generic list of all the pieces we will reference later
+pieces = []
 red_pieces = pygame.sprite.Group()
 black_pieces = pygame.sprite.Group()
 
@@ -81,9 +78,10 @@ for a in range(0, 2):
         j = 0
 
     for b in range(0, 4):
-        piece = Pawn(0, 0, RED_PAWN)
+        piece = Pawn(0, 0, 'red')
         piece.rect.center = board_reference[i][j].rect.center
         red_pieces.add(piece)
+        pieces.append(piece)
         j += 2
 
 for a in range(0, 2):
@@ -95,13 +93,11 @@ for a in range(0, 2):
         j = 0
 
     for b in range(0, 4):
-        piece = Pawn(0, 0, BLACK_PAWN)
+        piece = Pawn(0, 0, 'black')
         piece.rect.center = board_reference[i][j].rect.center
         black_pieces.add(piece)
+        pieces.append(piece)
         j += 2
-
-
-        
 
 # Method for drawing the screen
 def draw_window():
@@ -109,6 +105,7 @@ def draw_window():
   board.draw(WIN)
   red_pieces.draw(WIN)
   black_pieces.draw(WIN)
+  pygame.draw.rect(WIN, (0,0,0), cursor)
   pygame.display.flip()
   
 
@@ -117,11 +114,17 @@ def main():
 
   running = True
   while running:
+    pos = pygame.mouse.get_pos()
+    cursor.center = pos
     clock.tick(FPS)
     for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        running = False
-
+        if event.type == pygame.QUIT:
+            running = False 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for piece in pieces:
+                if piece.rect.colliderect(cursor):
+                    piece.move()
+                
     # We will be using this later when we implement moving pieces   
     keys_pressed = pygame.key.get_pressed()
     draw_window()
--- pieces.py
+++ pieces.py
@@ -1,6 +1,16 @@
 import pygame
 import os
 
+# Load and scale images for the pieces
+RED_PAWN_IMAGE = pygame.image.load(os.path.join('images', 'red-pawn.png'))
+RED_KING_IMAGE = pygame.image.load(os.path.join('images', 'red-king.png'))
+BLACK_PAWN_IMAGE = pygame.image.load(os.path.join('images', 'black-pawn.png'))
+BLACK_KING_IMAGE = pygame.image.load(os.path.join('images', 'black-king.png'))
+
+RED_PAWN = pygame.transform.scale(RED_PAWN_IMAGE, (60, 60))
+RED_KING = pygame.transform.scale(RED_KING_IMAGE, (60, 60))
+BLACK_PAWN = pygame.transform.scale(BLACK_PAWN_IMAGE, (60, 60))
+BLACK_KING = pygame.transform.scale(BLACK_KING_IMAGE, (60, 60))
 class CheckerSquare(pygame.sprite.Sprite):
     def __init__(self, x_pos, y_pos, tile):
         super().__init__()
@@ -11,17 +21,41 @@ class CheckerSquare(pygame.sprite.Sprite):
     def __str__(self):
         return f"Checker Square a position ({self.x_pos}, {self.y_pos})"
 
-
 class Pawn(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos, piece) -> None:
+    def __init__(self, x_pos, y_pos, color) -> None:
         super().__init__()
-        self.image = piece
+        self.color = color
+        if color == "red":
+            self.image = RED_PAWN
+        elif color == "black":
+            self.image = BLACK_PAWN
+        else:
+            Exception("Invalid Input: Please enter 'red' or 'black' for Pawn or King color attribute")
+
         self.rect = self.image.get_rect()
         self.rect.center = [x_pos, y_pos]
+
+    def move(self, board_reference):
+        pass
+        """cur_square = {}
+        for i in range(0, len(board_reference)):
+            for s in range(0, len(i)):
+                if board_reference[i][j].colliderect(self):
+                    cur_square["i"] = """
+
 
 class King(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos, piece) -> None:
+    def __init__(self, x_pos, y_pos, color) -> None:
         super().__init__()
-        self.image = piece
+        self.color = color
+        if color == "red":
+            self.image = RED_KING
+        elif color == "black": 
+            self.image = BLACK_KING
+        else:
+            Exception("Invalid Input: Please enter 'red' or 'black' for King color attribute")
         self.rect = self.image.get_rect()
         self.rect.center = [x_pos, y_pos]
+    
+    def move(self, board_reference):
+        pass

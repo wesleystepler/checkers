2b210e94964a82429960f004a360ff5be12d0ecb
Wesley Stepler <pwesleystep@outlook.com>
2023-06-12 09:38:53 -0400

    Double jumping in the same direction doesn't crash

--- checkers.py
+++ checkers.py
@@ -202,29 +202,56 @@ def cursor_on_piece(cursor, black_pieces, red_pieces, turn):
 
 
 def get_available_jumps(cur_piece, board_reference, pieces, i, j, possible_moves):
-    if (i+2) < len(board_reference) and (j+2) < len(board_reference[i]):
-        if not board_reference[i+2][j+2].occupied(pieces):
-            for p in pieces:
-                if p.rect.colliderect(board_reference[i+1][j+1]) and p.color != cur_piece.color:
-                    possible_moves.append(board_reference[i+2][j+2])
+    can_jump = True
+    i1, i2, i3, i4 = i, i, i, i
+    j1, j2, j3, j4 = j, j, j, j
+    while can_jump:
+        #print(i, j)
+        moves = 0
+        if (i1+2) < len(board_reference) and (j1+2) < len(board_reference[i1]):
+            if not board_reference[i1+2][j1+2].occupied(pieces):
+                for p in pieces:
+                    if p.rect.colliderect(board_reference[i1+1][j1+1]) and p.color != cur_piece.color:
+                        possible_moves.append(board_reference[i1+2][j1+2])
+                        moves += 1
+                        i1 += 2
+                        j1 += 2
+                        break
 
-    if (i+2) < len(board_reference) and (j-2) >= 0:
-        if not board_reference[i+2][j-2].occupied(pieces):
-            for p in pieces:
-                if p.rect.colliderect(board_reference[i+1][j-1]) and p.color != cur_piece.color:
-                    possible_moves.append(board_reference[i+2][j-2])
+        if (i2+2) < len(board_reference) and (j2-2) >= 0:
+            if not board_reference[i2+2][j2-2].occupied(pieces):
+                for p in pieces:
+                    #print(i2, j2)
+                    if p.rect.colliderect(board_reference[i2+1][j2-1]) and p.color != cur_piece.color:
+                        possible_moves.append(board_reference[i2+2][j2-2])
+                        moves += 1
+                        i2 += 2
+                        j2 -= 2
+                        break
 
-    if (i-2) >= 0 and (j+2) < len(board_reference[i]):
-        if not board_reference[i-2][j+2].occupied(pieces):
-            for p in pieces:
-                if p.rect.colliderect(board_reference[i-1][j+1]) and p.color != cur_piece.color:
-                    possible_moves.append(board_reference[i-2][j+2])
+        if (i3-2) >= 0 and (j3+2) < len(board_reference[i]):
+            if not board_reference[i3-2][j3+2].occupied(pieces):
+                for p in pieces:
+                    #print(i3, j3)
+                    if p.rect.colliderect(board_reference[i3-1][j3+1]) and p.color != cur_piece.color:
+                        possible_moves.append(board_reference[i3-2][j3+2])
+                        moves += 1
+                        i3 -= 2
+                        j3 += 2
+                        break
+                    
+        if (i4-2) >= 0 and (j4-2) >= 0:
+            if not board_reference[i4-2][j4-2].occupied(pieces):
+                for p in pieces:
+                    if p.rect.colliderect(board_reference[i4-1][j4-1]) and p.color != cur_piece.color:
+                        possible_moves.append(board_reference[i4-2][j4-2])
+                        moves += 1
+                        i4 -= 2
+                        j4 -= 2
+                        break
 
-    if (i-2) >= 0 and (j-2) >= 0:
-        if not board_reference[i-2][j-2].occupied(pieces):
-            for p in pieces:
-                if p.rect.colliderect(board_reference[i-1][j-1]) and p.color != cur_piece.color:
-                    possible_moves.append(board_reference[i-2][j-2])
+        if moves == 0:
+            can_jump = False
 
     return possible_moves
 
